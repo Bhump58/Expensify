d51a9ee5e138fe55d3c6ab6667603956734a4cba
Brian Humphrey <Humphreybrian58@gmail.com>
2017-11-08 17:46:37 -0500

    stripped to bear bones of poject

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
+
 import 'normalize.css/normalize.css';
 import './styles/styles.scss'
 
 
 const app = document.getElementById("app");
-ReactDom.render(<IndecisionApp />, app)
+ReactDom.render(<p>this is my boiler plate<p/>, app)
