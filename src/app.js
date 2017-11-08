adb4d7d727e3676cc37d08ca07c178973d0c2db3
Brian Humphrey <Humphreybrian58@gmail.com>
2017-11-08 17:15:36 -0500

    Expensify app

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss'
+
+
+const app = document.getElementById("app");
+ReactDom.render(<IndecisionApp />, app)
