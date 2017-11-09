a97b72ea2b156243c558ebc98c09b56d79d9fed1
Brian Humphrey <Humphreybrian58@gmail.com>
2017-11-09 16:03:48 -0500

    Added router

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
+import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
-import './styles/styles.scss'
+import './styles/styles.scss';
 
 
-const app = document.getElementById("app");
-ReactDom.render(<p>this is my boiler plate<p/>, app)
+
+ReactDom.render(<AppRouter />, document.getElementById("app"));
